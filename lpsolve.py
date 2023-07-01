622721c3b7be87fecf26829ef7435de178cb8ecf
Miles S <31926270+Baricus@users.noreply.github.com>
2023-07-01 16:14:33 -0500

    lp falloff approach

--- lpsolve.py
+++ lpsolve.py
@@ -34,17 +34,19 @@ def solve_lp(dataStruct, kis, m: gp.Model, gamma: np.float64, variables: npt.NDA
     :param features: np array of data points per model
     :return:
     """
-    # maybe this makes it faster?
-    m.tune()
-
-    # reset model, removing constraints if they exist
+    # reset model, and remove constraints if they exist
     m.reset()
     m.remove(m.getConstrs())
+
+    # set up parameters since reset resets it
+    m.Params.SolutionLimit = 1
+    m.Params.OutputFlag = 0
 
     N = len(features)
 
     sys.stdout.flush()
     print(f'testing feasability of gamma={gamma}', flush=True)
+    sys.stdout.flush()
     # we could probably build up the color constraints once and repeatedly add them
     # this builds up the lhs of the constraints in exprs
     exprs = defaultdict()
@@ -80,6 +82,7 @@ def solve_lp(dataStruct, kis, m: gp.Model, gamma: np.float64, variables: npt.NDA
     # if we're feasible, return true otherwise return false
     # model is passed back automatically
     # TODO: ensure else case is ONLY if model is feasible (error otherwise)
+    sys.stdout.flush()
     if m.status == GRB.INFEASIBLE or m.status == GRB.INF_OR_UNBD:
         print(f'Model for {gamma} is infeasible')
         return False
@@ -92,56 +95,11 @@ def solve_lp(dataStruct, kis, m: gp.Model, gamma: np.float64, variables: npt.NDA
         exit(-1)
 
 
-
-
-def bin_lpsolve(k):
-    # File fields
-    allFields = [
-        "age",
-        "workclass",
-        "fnlwgt",  # what on earth is this one?
-        "education",
-        "education-num",
-        "marital-status",
-        "occupation",
-        "relationship",
-        "race",
-        "sex",
-        "capital-gain",
-        "capital-loss",
-        "hours-per-week",
-        "native-country",
-        "yearly-income",
-    ]
-
-    # fields we care about for parsing
-    color_field = ['race', 'sex']
-    feature_fields = {'age', 'capital-gain', 'capital-loss', 'hours-per-week', 'fnlwgt', 'education-num'}
-
-    # variables for running LP bin-search
-
-    # binary search params
-
-    # the minimum quantization of our search space
-    epsilon = np.float64("0.01")
-
-    # coreset params
-    # Set the size of the coreset
-    coreset_size = 10000
-
-    # other things for gurobi
-    method = 2  # model method of solving
-
-    # start the timer
-    timer = utils.Stopwatch("Parse Data")
-
-    colors, features = utils.read_CSV("./datasets/ads/adult.data", allFields, color_field, '_', feature_fields)
-    assert (len(colors) == len(features))
-
+def bin_lpsolve(features, colors, k, epsilon, a):
     # all colors made by combining values in color_fields
     color_names = np.unique(colors)
 
-    timer.split("Coreset")
+    timer = utils.Stopwatch("Normalization")
 
     # "normalize" features
     # Should happen before coreset construction
@@ -149,25 +107,21 @@ def bin_lpsolve(k):
     devs  = features.std(axis=0)
     features = (features - means) / devs
 
-    timer.split("Normalization")
+    timer.split("Coreset")
 
-
-    print("[LPSOLVE] Number of points (original): ", len(features))
     d = len(feature_fields)
     m = len(color_names)
     features, colors = CORESET.Coreset_FMM(features, colors, k, m, d, coreset_size).compute()
-    print("[LPSOLVE] Number of points (coreset): ", len(features))
 
     N = len(features)
 
     timer.split("Building Kis")
 
-    kis = utils.buildKisMap(colors, k, 0.2)
+    kis = utils.buildKisMap(colors, k, a)
 
     # keys are appended using underscores
     # TODO: group formulas => max(1, (1-a) * k n_c / n)
     # NOTE: DO THIS AFTER CORESET
-    # a = 0.2
     # n = number of input points in color c
     # (k * n_c / n) <- is all we need!
 
@@ -180,11 +134,13 @@ def bin_lpsolve(k):
 
     # first we need to find the high value
     print('Solving for high bound')
-    high = 50  # I'm assuming it's a BIT larger than 0.01
+    high = 20  # I'm assuming it's a BIT larger than 0.01
     gamma = high * epsilon
     m = gp.Model(f"feasibility model")  # workaround for OOM error?
-    m.Params.method = method
     m.Params.SolutionLimit = 1
+    m.Params.OutputFlag = 0
+    m.Params.LogFile = ""
+    m.Params.LogToConsole = 0
 
     variables = m.addMVar(N, name="x", vtype=GRB.CONTINUOUS)
 
@@ -304,6 +260,135 @@ def bin_lpsolve(k):
 
     return diversity, total
 
+
+def epsilon_falloff(features, colors, k, epsilon, a):
+    """
+    starts at a high bound from the corest and repeatedly falls off by 1-epsilon
+    :param features:
+    :param colors:
+    :param k:
+    :param a:
+    :param epsilon:
+    :return:
+    """
+
+    # all colors made by combining values in color_fields
+    color_names = np.unique(colors)
+
+    timer = utils.Stopwatch("Normalization")
+
+    # "normalize" features
+    # Should happen before coreset construction
+    means = features.mean(axis=0)
+    devs  = features.std(axis=0)
+    features = (features - means) / devs
+
+    timer.split("Coreset")
+
+    d = len(feature_fields)
+    m = len(color_names)
+    corset = CORESET.Coreset_FMM(features, colors, k, m, d, coreset_size)
+    features, colors = corset.compute()
+
+    N = len(features)
+
+    timer.split("Building Kis")
+
+    kis = utils.buildKisMap(colors, k, a)
+
+    timer.split("Tree Creation")
+
+    # create data structure
+    data_struct = algo.create(features)
+
+    timer.split("Exponential falloff")
+
+    # build model
+    m = gp.Model("feasibility model")  # workaround for OOM error?
+    m.Params.SolutionLimit = 1
+    m.Params.OutputFlag = 0
+    m.Params.LogFile = ""
+    m.Params.LogToConsole = 0
+
+
+    gamma = corset.compute_gamma_upper_bound()
+
+    variables = m.addMVar(N, name="x", vtype=GRB.CONTINUOUS)
+
+    # fall off until solving
+    while not solve_lp(data_struct, kis, m, np.float_(gamma), variables, colors, features):
+        gamma = gamma * (1 - epsilon)
+
+    timer.split("Randomized Rounding")
+    # get results of the LP
+    vars = m.getVars()
+    X = np.array(m.getAttr("X", vars))
+    names = np.array(m.getAttr("VarName", vars))
+    assert(len(X) == N)
+
+    # do we want all points included or just the ones in S?
+    S = rand_round(gamma / 2, X, features, colors, kis)
+
+    res, total = timer.stop()
+
+    # compute diversity value of solution
+    solution = features[S]
+
+    diversity = utils.compute_maxmin_diversity(solution)
+    print(f'Solved diversity is {diversity}')
+
+    return diversity, total
+
+
 if __name__ == '__main__':
+    # setup
+    # File fields
+    allFields = [
+        "age",
+        "workclass",
+        "fnlwgt",  # what on earth is this one?
+        "education",
+        "education-num",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "capital-gain",
+        "capital-loss",
+        "hours-per-week",
+        "native-country",
+        "yearly-income",
+    ]
+
+    # fields we care about for parsing
+    color_field = ['race', 'sex']
+    feature_fields = {'age', 'capital-gain', 'capital-loss', 'hours-per-week', 'fnlwgt', 'education-num'}
+
+    # variables for running LP bin-search
+
+    # binary search params
+
+    # coreset params
+    # Set the size of the coreset
+    coreset_size = 10000
+
+    # other things for gurobi
+    method = 2  # model method of solving
+
+    colors, features = utils.read_CSV("./datasets/ads/adult.data", allFields, color_field, '_', feature_fields)
+    assert (len(colors) == len(features))
+
     # run some tests!
-    for i in range(10, 101):
+    results = []
+
+    # first for the proper 100
+    for k in range(10, 101, 10):
+        #div, time = bin_lpsolve(features, colors, k, 0.01, 0)
+        div, time = epsilon_falloff(features, colors, k, 0.05, 0)
+        results.append((k, div, time))
+
+    print('\n\nFINAL RESULTS:')
+    print('k,\tdiversity,\ttime,')
+    for k, div, time in results:
+        print(f'{k},\t{div},\t{time},')
