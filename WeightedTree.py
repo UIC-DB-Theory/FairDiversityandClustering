033864ffb2e8a4911dc782c1ec633b38a3d5bc61
J Wiseman <1478061+Harmoniguy@users.noreply.github.com>
2023-07-07 15:15:43 -0500

    First version of the weighted tree, needs further testing

--- WeightedTree.py
+++ WeightedTree.py
@@ -1,98 +1,94 @@
-import subprocess
-import numpy as np
 import json
+import subprocess
+
+import numpy as np
+
 
 class WeightedTree:
 
-    def __init__(self):
-
-        self.executable = './ParGeoCtl/pargeoctl'
-        self.cat = 'cat'
+    def __init__(self, dimension: int):
+        assert 1 < dimension < 8, "dimension is outside of acceptable values"
+        self.dim = dimension
+        self.executable = './ParGeoWeightedTree/build/example/TreeClient'
+        # self.executable = './ParGeoCtl/pargeoctl'
         # error value for calculated coreset
         self.proc = subprocess.Popen(
-            [self.executable],
+            [self.executable, str(dimension)],
+            # [self.executable],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE
         )
 
         self.points = np.array([])
-    
-    def construct_tree(self, points):
-        message_json = {
-            "type": "build-datastructure", 
-            "dimension": 3, 
-            "points": points.tolist()}
-        
-        message_str = json.dumps(message_json)
 
+    def send_message(self, message_str):
         self.proc.stdin.write(bytes(message_str + "\n", 'utf-8'))
         self.proc.stdin.flush()
         response_str = self.proc.stdout.readline().decode()
-        response_str = response_str[:-1] #strip the newline
+        response_str = response_str[:-1]  # strip the newline
+        return json.loads(response_str)
 
-        response_json = json.loads(response_str)
-        # TODO: check response and handle if errors occured
+    def construct_tree(self, points):
+        message_json = {
+            "type": "build-datastructure",
+            "dimension": self.dim,
+            "points": points.tolist()}
 
+        message_str = json.dumps(message_json)
+        response = self.send_message(message_str)
+        # TODO: check response and handle if errors occurred
+        return response
 
     def run_query(self, radius, weights):
         message_json = {
-            "type" : "run-query", 
-            "radius" : radius, 
-            "weights" : weights.tolist()}
-        
+            "type": "run-query",
+            "radius": radius,
+            "weights": weights.tolist()}
+
         message_str = json.dumps(message_json)
 
-        self.proc.stdin.write(bytes(message_str + "\n", 'utf-8'))
-        self.proc.stdin.flush()
-        response_str = self.proc.stdout.readline().decode()
-        response_str = response_str[:-1] #strip the newline
+        response_json = self.send_message(message_str)
+        # TODO: check response and handle if errors occurred
 
-        response_json = json.loads(response_str)
-        # TODO: check response and handle if errors occured
-
-        return (np.array(response_json["result"]),response_json["time"])
+        return response_json
 
     def delete_tree(self):
         message = {
             "type": "exit"}
-        
-        json_message= json.dumps(message)
+
+        json_message = json.dumps(message)
 
         self.proc.stdin.write(bytes(json_message + "\n", 'utf-8'))
         self.proc.stdin.flush()
-            
+
 
 # Example usage.
 if __name__ == "__main__":
-    
+
     # Initialize the tree and the subprocess.
-    tree = WeightedTree()
+    tree = WeightedTree(3)
 
-    # COnstruct the tree on the data.
-    tree.construct_tree(np.array([[0.0, 0.0 ,0.0],[1.0, 2.0 ,3.0]]))
+    # Construct the tree on the data.
+    tree_construction_response = tree.construct_tree(np.array([0.0, 0.0, 0.0,
+                                  1.0, 2.0, 3.0,
+                                  1.0, 2.0, 3.0,
+                                  1.0, 2.0, 3.0,
+                                  1.0, 2.0, 3.0,
+                                  1.0, 2.0, 3.0,
+                                  1.0, 2.0, 3.0,
+                                  1.0, 2.0, 3.0]))
 
-    # Sample computation.
-    for i in range(0,5):
-        print(i)
-    
+    print(f"Constructed a tree: {tree_construction_response}")
+
     # Run a query on the tree.
-    result, time = tree.run_query(1, np.array([1, 2]))
-    print(f'Result: {result}')
-    print(f'Time(s): {time}')
 
-    # More sample computation.
-    for i in range(0,5):
-        print(i)
-    
+    result1 = tree.run_query(1, np.array([1, 1, 1]))
+    print(f"Query 1 Result: {result1}")
+
     # Run a query.
-    result, time = tree.run_query(1, np.array([1, 2]))
-    print(f'Result: {result}')
-    print(f'Time(s): {time}')
 
+    result2 = tree.run_query(.5, np.array([1, 5, 10]))
+    print(f"Query 2 Result: {result2}")
 
     # Delete the tree. Ensures the process exits correctly
     tree.delete_tree()
-
-
-
-
