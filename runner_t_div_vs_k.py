984dc6ea29c2f9f2572d7b22c16fba37c373d93d
yashkurkure <yash.kurkure1903@gmail.com>
2023-10-06 03:50:52 -0500

    Report the size of input set

--- runner_t_div_vs_k.py
+++ runner_t_div_vs_k.py
@@ -193,6 +193,7 @@ for dataset_name in setup["datasets"]:
                 unique=setup["datasets"][dataset_name]['filter_unique']
             )
             setup["datasets"][dataset_name]['points_per_color'] = dataset['points_per_color']
+            setup["datasets"][dataset_name]['size'] = len(dataset['features'])
             write_results(setup, results, color_results)
             features = dataset['features']
             colors = dataset['colors']
