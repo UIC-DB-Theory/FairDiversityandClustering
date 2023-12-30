fb6a16f207ccbad78665ea8059b72b9c6e1603e8
yashkurkure <yash.kurkure1903@gmail.com>
2023-12-30 02:54:35 -0600

    All skyline plots

---
+++ gen_graphs.sh
@@ -0,0 +1,4 @@
+python3 plot_k.py ./experiments/result_equal_kis/final.json
+python3 plot_k.py ./experiments/result_equal_kis_mwu/final.json
+python3 plot_k.py ./experiments/result_proportional_kis/final.json
+python3 plot_k.py ./experiments/result_proportional_kis_mwu/final.json
