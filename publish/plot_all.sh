76863b0a08670f98c70eda7e8b8c7283ab484982
yashkurkure <yash.kurkure1903@gmail.com>
2024-01-17 17:04:19 -0600

    plots scripts for paper reprod

---
+++ publish/plot_all.sh
@@ -0,0 +1,5 @@
+python3 plot.py ./macro_exp_equal.json 0.2
+python3 plot.py ./macro_exp_prop.json 0.2
+python3 plot.py ./micro_exp_equal.json 1.2
+python3 plot.py ./micro_exp_prop.json 1.2
+python3 streamplot.py ./macro_stream_beer_review.json -nolegend
