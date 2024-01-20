0c456468683cbcdb62ce8d2318171813be13e23a
yashkurkure <yash.kurkure1903@gmail.com>
2024-01-20 15:33:37 -0600

    yelp dataset results

--- publish/plot_all.sh
+++ publish/plot_all.sh
@@ -3,3 +3,4 @@ python3 plot.py ./macro_exp_prop.json 0.2
 python3 plot.py ./micro_exp_equal.json 1.2
 python3 plot.py ./micro_exp_prop.json 1.2
 python3 streamplot.py ./macro_stream_beer_review.json -nolegend
+python3 streamplot.py ./macro_stream_yelp.json -nolegend
