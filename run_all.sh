8a2023f4f6208cde6e226782def071450b280948
yashkurkure <yash.kurkure1903@gmail.com>
2024-01-20 16:23:48 -0600

    scripts to reproduce

---
+++ run_all.sh
@@ -0,0 +1,6 @@
+python3 runner_k.py ./publish/setup/macro_exp_equal.json
+python3 runner_k.py ./publish/setup/macro_exp_prop.json
+python3 runner_k.py ./publish/setup/micro_exp_prop.json
+python3 runner_k.py ./publish/setup/macro_exp_equal.json
+python3 streamrunner_k.py ./publish/setup/macro_stream_beer_review.json
+python3 streamrunner_k.py ./publish/setup/macro_stream_yelp.json
