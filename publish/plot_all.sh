9806bdd978faf25fd742c1994916099f9f6fe8c5
Miles Shamo <milesshamo@gmail.com>
2024-01-21 01:06:57 -0600

    Cleanup of removed scripts

--- publish/plot_all.sh
+++ publish/plot_all.sh
@@ -3,4 +3,3 @@ python3 plot.py ./results/macro_exp_prop.json 0.2
 python3 plot.py ./results/micro_exp_equal.json 1.2
 python3 plot.py ./results/micro_exp_prop.json 1.2
 python3 streamplot.py ./results/macro_stream_beer_review.json -nolegend
-python3 streamplot.py ./results/macro_stream_yelp.json -nolegend
