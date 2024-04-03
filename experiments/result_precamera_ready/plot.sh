1faff0abffba93d9681920e5fcaaf6dd407b09ba
yashkurkure <yash.kurkure1903@gmail.com>
2024-04-03 16:03:30 -0500

    stream results

---
+++ experiments/result_precamera_ready/plot.sh
@@ -0,0 +1,3 @@
+python3 ./publish/streamplot.py ./experiments/result_precamera_ready/result_beer.json -nolegend
+python3 ./publish/streamplot.py ./experiments/result_precamera_ready/result_popsim_1m.json -nolegend
+python3 ./publish/streamplot.py ./experiments/result_precamera_ready/result_popsim.json -nolegend
